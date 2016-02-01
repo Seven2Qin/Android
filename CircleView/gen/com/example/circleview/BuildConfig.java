3b9fa8c8b68d78e808324a085ef51b3809de19d7
shengwei.qin <shengwei.qin@jrdcom.com>
2016-02-01 10:36:36 +0800

    添加CircleView

---
+++ CircleView/gen/com/example/circleview/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.circleview;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ CircleView/src/com/example/circleview/circlelayout/MyCircleLayoutItem.java
@@ -0,0 +1,60 @@
+package com.example.circleview.circlelayout;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.AttributeSet;
+import android.widget.Toast;
+
+
+
+public class MyCircleLayoutItem extends CircularLayoutItem {
+
+
+
+    public MyCircleLayoutItem(Context context) {
+        super(context);initialize();
+    }
+
+    public MyCircleLayoutItem(Context context, CircleLayout cl) {
+        super(context, cl);
+        initialize();
+    }
+
+    public MyCircleLayoutItem(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize();
+    }
+
+    public MyCircleLayoutItem(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initialize();
+    }
+
+    public void initialize()
+    {
+
+
+        this.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick() {
+                Toast.makeText(getContext(),"Item number: "+getIndex(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        this.setOnFocusListener(new OnFocusListener()
+        {
+            @Override
+            public void onFocus() {
+//                Toast.makeText(getContext(),"Item number: "+getIndex()+ " is now on focus ",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onUnFocus() {
+
+            }
+        });
+    }
+
+
+
+}
